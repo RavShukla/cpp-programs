2f3c864a6e51b0cc9618c063d5cee0b426385d38
Gaurav <gauravshukla2006ldh@gmail.com>
2025-06-20 15:51:59 +0530

    specificataion

--- addition of first and reverse no..cpp
+++ addition of first and reverse no..cpp
@@ -2,18 +2,18 @@
 using namespace std;
 int main() {
  //program for reverse no. and first no. addition
- int rvn=0 ;
+ int rvn=0 ;//initialisation
  int nv;
  cout <<"enter a no.";
- cin>>nv;
+ cin>>nv;//input 
  int n = nv;
- while(n!=0){
+ while(n!=0){ //looping
  int ld = n%10 ;
  rvn = rvn * 10;
  rvn = rvn +  ld ;
  n = n/10;
  }
- cout<<"the reverse no. is :" ;
+ cout<<"the reverse no. is :" ;// output
  cout << rvn<<endl;
  cout << "sum of first and reverse no." ;
  cout << nv+rvn<<endl;
