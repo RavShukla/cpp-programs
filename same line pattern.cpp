458a856546fa2fc48c34278c897c968755d9062d
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-06-21 01:53:06 +0530

    pattern printing using nested loops
    This loop print pattern using nested loops

---
+++ same line pattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// printing variable solid rectangle 
+ int m ;
+ cout << "enter no. of rows";
+ cin >> m;
+ 
+ int n ;
+ cout << "enter no. of columns";
+ cin >> n;
+ 
+ for(int i=1 ; i<=m ; i++){
+ 	
+ 	
+ 	for(int j=1 ; j<=n ; j++ ){
+ 		char ch = char(64+i);
+ 		
+ 		cout<<ch;
+	 }
+	 
+	 cout<< endl ;
+ }
+ 
+ 
+}
