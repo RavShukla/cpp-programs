0fb19fc245f7c35ea832e3cc3bad294d9180a015
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-06-23 19:30:55 +0530

    printing Floyd triangle using loops

---
+++ floyd triangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//reverse star triangle 
+ int m ;
+ cout << "enter no. of rows";
+ cin >> m;
+ int a = 1 ;
+ 
+ for(int i=1 ; i<=m ; i++){
+ 	
+ 	
+ 	for(int j=1 ; j <= i ; j++ ){
+ 		
+ 		cout<<a<<" ";
+ 		cout<<a++;
+ 	
+ 		
+	 }
+	 
+	 cout<< endl ;
+ 
+}
+}
