ab9dc8d9514bdd8601283ab30600f2228dc9fb07
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-06-23 19:31:30 +0530

    reverse flip star using loop

---
+++ reverse hor flip star.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//reverse star triangle 
+ int m ;
+ cout << "enter no. of rows";
+ cin >> m;
+ 
+ for(int i=1 ; i<=m ; i++){
+ 	
+ 	
+ 	for(int j=1 ; j <= m-i+1; j++ ){
+ 		
+ 		cout<<i;
+ 		
+	 }
+	 
+	 cout<< endl ;
+ 
+}
+}
