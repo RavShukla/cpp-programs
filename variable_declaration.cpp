f8f660937894fdb929aeb027d1fccd8e6bb9a872
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-06-23 19:32:09 +0530

    variable declaration in c++

---
+++ variable_declaration.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//three ways to declare variable
+int main() {
+ int x = 5;
+ int y = 6 ;
+ cout<<x+y << endl;
+ 
+
+ int g,s ;
+ g = 7;
+ s = 8;
+ cout<< g+s << endl ;
+
+
+ int a = 10 , b = 9 ;
+ cout << a + b << endl;
+//overwriting of variable we changed the value of x in the program from 5 to 23
+ x = 23;
+ cout<< x << endl ;
+}
