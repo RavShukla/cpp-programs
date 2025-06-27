ffc184617e00555b75fb67f2edc20f966b63329b
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-06-28 02:52:55 +0530

    A program which tells about functions

---
+++ functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void sum (int a , int b ){
+	cout << a+b ;
+}
+int main() {
+ int a;
+ cout<<"enter first no. : ";
+ cin>>a;
+ 
+ int b;
+ cout<<"enter second no. : ";
+ cin>>b;
+ 
+ sum(a,b);
+ 
+ 
+ 
+}
