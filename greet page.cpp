01b4e1571091f648c9629de9a56dd2b0d5884efd
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-06-29 03:06:23 +0530

    greet page code

---
+++ greet page.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void greet (string a){
+	cout << "Hello " << a << " have a nice day";
+}
+int main() {
+ string  a;
+ cout<<"enter your name  : ";
+ cin>>a;
+ 
+ 
+ 
+ greet(a);
+ 
+ 
+ 
+}
