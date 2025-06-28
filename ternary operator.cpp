46d5380c7feda44f5a793bd3ca75587b5172c8a5
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-06-29 02:57:56 +0530

    another way of writing if else statement

---
+++ ternary operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int n ;
+ cout<<"enter no.";
+ cin >> n ;
+ // syntax (condition ) ? if true : if false ;
+ 
+ (n%2==0)?cout<<"even no.": cout << "odd no.";
+}
