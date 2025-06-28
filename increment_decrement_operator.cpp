d828f19bec3a2d97c88beb0f5879177e516496d7
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-06-29 03:00:54 +0530

    increment decrement operator

---
+++ increment_decrement_operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int x= 5 ;
+ x++;//post increment operator
+ cout<<x<<endl;
+
+ x--;//post decrement operaor
+ cout<<x<<endl;
+
+ ++x;//pre increment operator
+ cout<<x<<endl;
+
+ --x;//pre decrement operator
+ cout<<x<<endl;
+
+
+}
