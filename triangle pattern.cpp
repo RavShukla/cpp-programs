f4573ca1a40c0073e6e7e7f4f7fb0dd0622779de
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2025-07-16 12:56:18 +0530

    pattern printing

---
+++ triangle pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// printing variable solid rectangle 
+ int m ;
+ cout << "enter no. of rows";
+ cin >> m;
+ 
+ 
+ 
+ for(int i=1 ; i<=m ; i++){
+ 	
+ 	for(int j=1 ; j<=i ; j++ ){
+ 		
+ 		cout<<"* ";
+	 }
+	 
+	 cout<< endl ;
+ }
+ 
+ 
+}
